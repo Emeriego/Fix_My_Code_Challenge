c9ef4f7428f7c545feee2a0a0b8cdfbfc4c565b4
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-01-23 12:50:05 +0100

    task 4

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -28,17 +28,19 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     }
 
     // Traverse to the node before the one at the specified index
-    for (i = 0; i < index - 1 && current != NULL; i++)
+    for (i = 0; i < index && current != NULL; i++)
         current = current->next;
 
     // Check if index is out of bounds
-    if (current == NULL || current->next == NULL)
+    if (current == NULL)
         return (-1);
 
     // Update pointers to skip the current node
-    current->next = current->next->next;
+    if (current->prev)
+        current->prev->next = current->next;
     if (current->next)
-        current->next->prev = current;
+        current->next->prev = current->prev;
 
+    free(current);
     return (1);
 }

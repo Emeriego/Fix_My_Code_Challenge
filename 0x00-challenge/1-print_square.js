591ad9c9cf028ec064c3f31c72b87163d06b2405
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-01-23 11:34:20 +0100

    task 1

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,26 +1,22 @@
 #!/Users/apple/.nvm/versions/node/v17.9.1/bin/node
 /*
     Print a square with the character #
-    The size of the square must be the first argument 
+
+    The size of the square must be the first argument
     of the program.
 */
 
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.stderr.write("Usage: /Users/apple/.nvm/versions/node/v17.9.1/bin/node 1-print_square.js <size>\n");
+    process.stderr.write("Example: /Users/apple/.nvm/versions/node/v17.9.1/bin/node 1-print_square.js 8\n");
     process.exit(1);
 }
 
-const size = parseInt(process.argv[2]);
+size = parseInt(process.argv[2], 10);
 
-if (isNaN(size) || size <= 0) {
-    process.stderr.write("Invalid size. Please provide a positive integer.\n");
-    process.exit(1);
-}
-
-for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
